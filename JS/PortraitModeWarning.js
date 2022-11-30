edc4406b3f068939de59340aadae21a895e0f756
CSchepi <74829152+CSchepi@users.noreply.github.com>
2022-11-30 18:03:42 +0100

    In Deutsch üebrsetzen

--- JS/PortraitModeWarning.js
+++ JS/PortraitModeWarning.js
@@ -37,11 +37,11 @@ function ShowOverlay(){
     if(blockeritem==null){
         document.body.innerHTML += "<div id='PortraitBlock' style='position: absolute;top:0;left:0;z-index:100;width: 100%;height: 100%;background: rgba(255,245,230,0.9);text-align: center;'>\n"+
     "   <div id='PortraitBlockContent' style='position: absolute; border: 2px solid black;border-radius:1vw;width: 50%;height: 50%;top:25%;left:25%'>\n"+
-    "       <p>Hi! The following settings are highly recommended:</p>\n"+
-    "          <h2>Use in landscape mode</h2>  \n"+
-    "           <img src='../img/Icons/RotatePhone.gif' width='40%'> \n"+
-    "      <a style='position:absolute;left:1vw;bottom:5px;font-size: 2vw;'><input id='PortraitCheckBox' type='checkbox'>Don't show again!</a>\n"+
-    "     <a style='position:absolute;right:1vw;bottom:5px;font-size:2vw;cursor: pointer;text-decoration: underline;' onclick='skip()'>skip>></a> \n"+
+    "       <p>Hi! Die folgenden Einstellungen werden empfohlen:</p>\n"+
+    "          <h2>Im Querformat nutzen</h2>  \n"+
+    "           <img src='./img/Icons/RotatePhone.gif' width='40%'> \n"+
+    "      <a style='position:absolute;left:0vw;bottom:5px;font-size: 2vw;'><input id='PortraitCheckBox' style='width:2vw' type='checkbox'>Nicht nochmal anzeigen!</a>\n"+
+    "     <a style='position:absolute;right:1vw;bottom:5px;font-size:2vw;cursor: pointer;text-decoration: underline;' onclick='skip()'>Überspringen>></a> \n"+
     " </div>\n"+ 
     "</div>";
     }
